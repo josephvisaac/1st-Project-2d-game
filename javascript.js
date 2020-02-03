d86e74db98c61fa5313600409a8a67804a83d0d4
Joseph <josephvisaac@gmail.com>
2020-02-03 14:50:03 -0500

    ffd

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ function startGame() {
     ctx.fillStyle="blue";
 
     //speed start
-    ctx.speedY = 2;
+    ctx.speedY = 3;
     //
     ctx.x = 30;         //sizes
     ctx.y = 30;         
@@ -34,14 +34,35 @@ function startGame() {
 
 
                 function newPos(){
-                ctx.py += ctx.speedY;
+                        
+                        if(ctx.py <= 0 ){
+                            ctx.speedY = 2;
+                            ctx.py = 1;
+                        }
+                     else if(ctx.py >= 460 ){
+                        ctx.speedY = -2;
+                        ctx.py = 458;
+                    }
+                      ctx.py += ctx.speedY;
+                    
             }
+
                 function moveup() {
-                ctx.speedY -= 2;
+                        if(ctx.speedY <= 3 && ctx.speedY > 0){ ctx.speedY -= 4}
+                         
+                         else if(ctx.speedY == -3){ ctx.speedY = ctx.speedY}
+                         else ctx.speedY = -2
+                        
+                        
+                        
+                        
+                    
+                
             }
   
                 function movedown() {
-                    ctx.speedY += 2;
+                    if(ctx.speedY <= 3){ ctx.speedY = 2}
+                    else ctx.speedY += 2;
                 }
 
                 document.onkeydown = function (e) {  
@@ -56,12 +77,86 @@ function startGame() {
 
                 }
 
+
+
+
+                function getRandomArbitrary(min, max) { return Math.random() * (max - min) + min; }
+let r = getRandomArbitrary(240, 550)
+let x = 0;
+
+let empty = [];
+function stamp() {
+    empty.forEach(sq => {
+        
+        // ctx.fillStyle = sq.color
+        ctx.fillRect(sq.x--, sq.y, sq.width, sq.height);
+    })
+
+}
+
+function redo() {
+    setInterval(() => {
+        let sq = {
+            x: 860,
+            y: getRandomArbitrary(0, 480),
+            width: 45,
+            height: 300,
+            color: 'red'
+        }
+        empty.push(sq)
+    }, 4000)
+
+}
+redo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             function animate () {
                 let loop = window.requestAnimationFrame(animate)
                 ctx.clearRect(0, 0, canvas.width, canvas.height)
                 newPos()
                 ctx.fillRect(ctx.px, ctx.py, ctx.x, ctx.y);
-
+                
+                stamp()
 
                 }
                 
